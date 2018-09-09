20515b62d0431485a4bcc5fdaa9dc647837dca5e
Vladimir <vova.tkachenko.11@mail.ru>
2018-09-09 21:27:45 +0300

    test

---
+++ creativia/js/script.js
@@ -0,0 +1,15 @@
+/*<!--============ Плавный переход к якорю ===========-->*/
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+	});
+	
+	$(".accordeon .p").hide().prev().click(function() {
+		$(this).parents(".accordeon").find(".p").not(this).hide(500).prev().removeClass("active");
+		$(this).next().not(":visible").show(500).prev().addClass("active");
+	});
+
+});
